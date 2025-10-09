e2eb3816039a4c36e28a4180c21cbd65d17d21f6
apptie <appti@naver.com>
2025-10-09 19:15:50 +0900

    chore: 프로젝트 초기 세팅

---
+++ pekko/src/main/java/com/tok/pekko/PekkoApplication.java
@@ -0,0 +1,12 @@
+package com.tok.pekko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PekkoApplication.class, args);
+	}
+}
---
+++ pekko/src/test/java/com/tok/pekko/PekkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tok.pekko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PekkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
