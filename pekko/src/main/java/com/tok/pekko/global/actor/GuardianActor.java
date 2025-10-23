6dcfe9a6ba2364c2e14d5ab981e67fd68351b6c7
apptie <appti@naver.com>
2025-10-23 10:21:08 +0900

    fix: GuardianActor가 SpawnProtocol.Comman Behavior를 반환하도록 변경
    - AbstractBehavior 상속 삭제

--- pekko/src/main/java/com/tok/pekko/global/actor/GuardianActor.java
+++ pekko/src/main/java/com/tok/pekko/global/actor/GuardianActor.java
@@ -2,23 +2,14 @@ package com.tok.pekko.global.actor;
 
 import org.apache.pekko.actor.typed.Behavior;
 import org.apache.pekko.actor.typed.SpawnProtocol;
-import org.apache.pekko.actor.typed.javadsl.AbstractBehavior;
-import org.apache.pekko.actor.typed.javadsl.ActorContext;
 import org.apache.pekko.actor.typed.javadsl.Behaviors;
-import org.apache.pekko.actor.typed.javadsl.Receive;
 
-public class GuardianActor extends AbstractBehavior<SpawnProtocol.Command> {
+public class GuardianActor {
 
     public static Behavior<SpawnProtocol.Command> create() {
-        return Behaviors.setup(GuardianActor::new);
+        return Behaviors.setup(context -> SpawnProtocol.create());
     }
 
-    private GuardianActor(ActorContext<SpawnProtocol.Command> context) {
-        super(context);
-    }
-
-    @Override
-    public Receive<SpawnProtocol.Command> createReceive() {
-        return newReceiveBuilder().build();
+    private GuardianActor() {
     }
 }
