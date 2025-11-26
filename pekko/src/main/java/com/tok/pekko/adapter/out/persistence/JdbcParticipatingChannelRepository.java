dc7fa16b6d6e52581cc8506d6d3ab42f046886a1
apptie <appti@naver.com>
2025-11-26 18:53:04 +0900

    feat: 사용자가 참여하고 있는 채널 조회 Repository 구현체 추가

--- pekko/src/main/java/com/tok/pekko/adapter/out/persistence/JdbcParticipatingChannelRepository.java
+++ pekko/src/main/java/com/tok/pekko/adapter/out/persistence/JdbcParticipatingChannelRepository.java
@@ -1,14 +1,32 @@
 package com.tok.pekko.adapter.out.persistence;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class JdbcParticipatingChannelRepository implements ParticipatingChannelRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public List<Long> findAllChannelIds(Long userId) {
-        // NO-OP
-        return List.of();
+        String sql = """
+                SELECT channel_id
+                FROM channel_memberships
+                WHERE user_id = :userId
+                """;
+
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("userId", userId);
+
+        return jdbcTemplate.query(
+                sql,
+                params,
+                (rs, rowNum) -> rs.getLong("channel_id")
+        );
     }
 }
